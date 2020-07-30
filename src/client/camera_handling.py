f96e3f6588cd278b6c6cb9cb9b59dec126e71d05
ManonMic <manon.michelet5@gmail.com>
2020-07-30 17:04:15 +0200

    Add function write_video_stream which permits to save a video stream into .avi file

--- src/client/camera_handling.py
+++ src/client/camera_handling.py
@@ -19,3 +19,24 @@ def display_video_stream(camera_url: str):
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+def write_video_stream(camera_url: str):
+    cap = cv2.VideoCapture(camera_url)
+    assert cap.isOpened(), "Error opening video stream"
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    out = cv2.VideoWriter('saved_video_stream/outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+
+    cv2.destroyAllWindows()
--- tests/test_camera_handling.py
+++ tests/test_camera_handling.py
@@ -1,8 +1,13 @@
 import pytest
-from client.camera_handling import display_video_stream
+from client.camera_handling import display_video_stream, write_video_stream
 
 
-def test_capturing_video():
+def test_display_video_stream():
     camera_url = "rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa"  # Public video stream
     # camera_url = "rtsp://192.168.1.37:8554/screen"
     display_video_stream(camera_url)
+
+
+def test_write_video_stream():
+    camera_url = "rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa"  # Public video stream
+    write_video_stream(camera_url)
