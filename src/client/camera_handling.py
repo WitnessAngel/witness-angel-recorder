231af4910a307273ace1bbc6c5f73ab47249035a
ManonMic <manon.michelet5@gmail.com>
2020-08-05 10:58:14 +0200

    Add class VideoStream

--- src/client/camera_handling.py
+++ src/client/camera_handling.py
@@ -1,42 +1,51 @@
 import cv2
 import numpy as np
 import logging
+import pytest
 
 logger = logging.getLogger()
 
 
-def display_video_stream(camera_url: str):
-    cap = cv2.VideoCapture(camera_url)
-    assert cap.isOpened(), "Error opening video stream"
+class VideoStream:
+    def __init__(self, video_stream_url=None):
+        self.video_stream_url = video_stream_url
 
-    while cap.isOpened():
-        ret, frame = cap.read()
+    def display_video_stream(self):
+        cap = cv2.VideoCapture(self.video_stream_url)
 
-        cv2.imshow('frame', frame)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                cv2.destroyAllWindows()
+                raise ValueError
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cap.release()
+        cv2.destroyAllWindows()
 
-    cap.release()
-    cv2.destroyAllWindows()
+    def write_video_stream(self):
+        cap = cv2.VideoCapture(self.video_stream_url)
+        # assert cap.isOpened(), "Error opening video stream"
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+        out = cv2.VideoWriter(
+            'saved_video_stream/outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height)
+        )
 
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                cv2.destroyAllWindows()
+                raise ValueError
+            out.write(frame)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-def write_video_stream(camera_url: str):
-    cap = cv2.VideoCapture(camera_url)
-    assert cap.isOpened(), "Error opening video stream"
-
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-    out = cv2.VideoWriter('saved_video_stream/outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        out.write(frame)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    out.release()
-
-    cv2.destroyAllWindows()
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
--- tests/test_camera_handling.py
+++ tests/test_camera_handling.py
@@ -1,13 +1,14 @@
 import pytest
-from client.camera_handling import display_video_stream, write_video_stream
+from client.camera_handling import VideoStream
 
 
 def test_display_video_stream():
     camera_url = "rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa"  # Public video stream
-    # camera_url = "rtsp://192.168.1.37:8554/screen"
-    display_video_stream(camera_url)
+    video_stream = VideoStream(video_stream_url=camera_url)
+    video_stream.display_video_stream()
 
 
 def test_write_video_stream():
     camera_url = "rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa"  # Public video stream
-    write_video_stream(camera_url)
+    video_stream = VideoStream(video_stream_url=camera_url)
+    video_stream.write_video_stream()
