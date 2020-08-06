dbc0ff15b719b5f93121b193bde4cb39b098161f
ManonMic <manon.michelet5@gmail.com>
2020-08-06 17:20:44 +0200

    Add documentation.

--- src/client/camera_handling.py
+++ src/client/camera_handling.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import logging
 import pytest
+import datetime
 
 logger = logging.getLogger()
 
@@ -11,8 +12,12 @@ class VideoStream:
         self.video_stream_url = video_stream_url
         self.on_pause = False
         self.quit = False
+        self.time = datetime.time(0, 0, 0)
 
     def display_video_stream(self):
+        """
+        Permits to show on a window the video stream.
+        """
         cap = cv2.VideoCapture(self.video_stream_url)
 
         while True:
@@ -29,6 +34,10 @@ class VideoStream:
         cv2.destroyAllWindows()
 
     def write_video_stream(self):
+        """
+        Save as an .avi file the video stream in saved_video_stream directory. If the state of the video stream is
+        on pause, images won't be saved until the state changes.
+        """
         cap = cv2.VideoCapture(self.video_stream_url)
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
@@ -42,7 +51,7 @@ class VideoStream:
         while cap.isOpened():
             self.get_state()
             ret, frame = cap.read()
-            if not ret:
+            if not ret:  # Nothing can be read from cap
                 cap.release()
                 cv2.destroyAllWindows()
                 raise ValueError
@@ -57,6 +66,9 @@ class VideoStream:
         cv2.destroyAllWindows()
 
     def get_state(self):
+        """
+        Changes state of the video stream according to condition (WILL CHANGE WHEN GUI WILL BE DONE).
+        """
         key_pressed = cv2.waitKey(1) & 0xFF
         if key_pressed == ord("p"):
             self.on_pause = True
--- src/client/ciphering_toolchain.py
+++ src/client/ciphering_toolchain.py
@@ -5,6 +5,16 @@ from wacryptolib.container import encrypt_data_into_container, decrypt_data_from
 def encrypt_video_stream(
     path: str, conf: dict, keychain_uid=None, metadata=None
 ) -> dict:
+    """
+    Put the video stream data saved in an .avi files into a container
+
+    :param path: str with relative path to .avi video stream file
+    :param conf: dictionary with entire configuration tree
+    :param keychain_uid: uuid for the set of encryption keys used
+    :param metadata: additional data to store unencrypted in container
+
+    :return: dictionary which contains every information to decrypt container
+    """
     with open(path, "rb") as video_stream:
         data = video_stream.read()
 
@@ -16,5 +26,12 @@ def encrypt_video_stream(
 
 
 def decrypt_video_stream(container: dict) -> bytes:
+    """
+    Decipher container where a video stream saved as an .avi file has been ciphered
+
+    :param container: dictionary which contains every information to decrypt container, returned by encrypt_video_stream
+
+    :return: initial data decrypted
+    """
     initial_data = decrypt_data_from_container(container=container)
     return initial_data
