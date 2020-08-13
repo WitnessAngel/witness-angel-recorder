8c84bb234209d73c3b0810c17c9de1a3163623ed
ManonMic <manon.michelet5@gmail.com>
2020-08-13 16:01:07 +0200

    Add parameter 'timeout' to VideoStream class to limit duration of a .avi file containing video stream recorded.

--- src/client/camera_handling.py
+++ src/client/camera_handling.py
@@ -2,29 +2,39 @@ import cv2
 import numpy as np
 import logging
 import pytest
+import os
+import time
 
 logger = logging.getLogger()
 
 
 class VideoStream:
-    def __init__(self, video_stream_url=None):
+    def __init__(self, timeout, video_stream_url=None):
         self.video_stream_url = video_stream_url
         self.on_pause = False
         self.quit = False
+        self.timeout = timeout
 
     def display_video_stream(self):
         cap = cv2.VideoCapture(self.video_stream_url)
+        if not cap.isOpened():
+            logger.debug("Opening video capture")
+            cap.open()
 
-        while True:
+        while cap.isOpened():
             ret, frame = cap.read()
+
             if not ret:
+                logger.debug("No image retrieved")
                 cap.release()
                 cv2.destroyAllWindows()
                 raise ValueError
+
             cv2.imshow("frame", frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
+        logger.debug("Closing video capture")
         cap.release()
         cv2.destroyAllWindows()
 
@@ -32,28 +42,52 @@ class VideoStream:
         cap = cv2.VideoCapture(self.video_stream_url)
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
-        out = cv2.VideoWriter(
-            "saved_video_stream/outpy.avi",
-            cv2.VideoWriter_fourcc("M", "J", "P", "G"),
-            10,
-            (frame_width, frame_height),
-        )
 
+        if not os.path.isdir("saved_video_stream"):
+            logger.debug("Creating directory 'saved_video_stream'")
+            os.mkdir("saved_video_stream")
+
+        fps = 10
+        out = self.change_recording_file(
+            frame_width=frame_width, frame_height=frame_height
+            )
+
+        if not cap.isOpened():
+            cap.open()
+        frame_count = 0
         while cap.isOpened():
             self.get_state()
             ret, frame = cap.read()
+
             if not ret:
+                logger.debug("No image retrieved")
                 cap.release()
                 cv2.destroyAllWindows()
                 raise ValueError
+
             if not self.on_pause:
                 out.write(frame)
+
+            frame_count += 1
+            duration = frame_count/fps
+            if duration >= self.timeout:
+                logger.debug("Closing video writer")
+                out.release()
+                out = self.change_recording_file(
+                    frame_width=frame_width, frame_height=frame_height 
+                )
+                frame_count = 0
+
             cv2.imshow("frame", frame)
-            if self.quit:  # quit
+            if self.quit:
                 break
 
+        logger.debug("Closing video capture")
         cap.release()
+
+        logger.debug("Closing video writer")
         out.release()
+
         cv2.destroyAllWindows()
 
     def get_state(self):
@@ -64,3 +98,17 @@ class VideoStream:
             self.on_pause = False
         if key_pressed == ord("q"):
             self.quit = True
+
+    def change_recording_file(self, frame_width, frame_height):
+        logger.debug("Changing recording file")
+        filename = "saved_video_stream/" + str(time.strftime("%m-%d-%Y_%H-%M-%S")) + ".avi"
+        fps = 10
+
+        out = cv2.VideoWriter(
+            filename,
+            cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+            fps,
+            (frame_width, frame_height),
+        )
+        
+        return out
--- tests/test_camera_handling.py
+++ tests/test_camera_handling.py
@@ -6,7 +6,7 @@ def test_display_video_stream():
     camera_url = (
         "rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa"
     )  # Public video stream
-    video_stream = VideoStream(video_stream_url=camera_url)
+    video_stream = VideoStream(10.0, video_stream_url=camera_url)
     video_stream.display_video_stream()
 
 
@@ -14,5 +14,5 @@ def test_write_video_stream():
     camera_url = (
         "rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa"
     )  # Public video stream
-    video_stream = VideoStream(video_stream_url=camera_url)
+    video_stream = VideoStream(10.0, video_stream_url=camera_url)
     video_stream.write_video_stream()
