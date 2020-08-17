46065aa589cb5e6d46c17218c59fdb5ab6ab4217
ManonMic <manon.michelet5@gmail.com>
2020-08-17 09:21:15 +0200

    Fix ciphering algorithm

--- src/client/ciphering_toolchain.py
+++ src/client/ciphering_toolchain.py
@@ -1,37 +1,36 @@
 from wacryptolib.encryption import encrypt_bytestring
 from wacryptolib.container import encrypt_data_into_container, decrypt_data_from_container
+from wacryptolib.encryption import encrypt_bytestring, decrypt_bytestring
 
 
 def encrypt_video_stream(
-    path: str, conf: dict, keychain_uid=None, metadata=None
+    path: str, encryption_algo: str, key
 ) -> dict:
     """
     Put the video stream data saved in an .avi files into a container
 
+    :param key: assymetric key which must be used to encrypt video stream
+    :param encryption_algo: name of algorithm used to encrypt
     :param path: str with relative path to .avi video stream file
-    :param conf: dictionary with entire configuration tree
-    :param keychain_uid: uuid for the set of encryption keys used
-    :param metadata: additional data to store unencrypted in container
 
     :return: dictionary which contains every information to decrypt container
     """
     with open(path, "rb") as video_stream:
         data = video_stream.read()
 
-    assert isinstance(data, bytes)
-    cipherdict = encrypt_data_into_container(
-        data=data, conf=conf, keychain_uid=keychain_uid, metadata=metadata
-    )
+    cipherdict = encrypt_bytestring(plaintext=data, encryption_algo=encryption_algo, key=key)
     return cipherdict
 
 
-def decrypt_video_stream(container: dict) -> bytes:
+def decrypt_video_stream(cipherdict: dict, encryption_algo: str, key) -> bytes:
     """
     Decipher container where a video stream saved as an .avi file has been ciphered
 
-    :param container: dictionary which contains every information to decrypt container, returned by encrypt_video_stream
+    :param key: private key from initial assymetric keypair
+    :param encryption_algo: name of algorithm used to decrypt
+    :param cipherdict: dictionary which contains every information to decrypt data, returned by encrypt_video_stream
 
     :return: initial data decrypted
     """
-    initial_data = decrypt_data_from_container(container=container)
+    initial_data = decrypt_bytestring(cipherdict=cipherdict, encryption_algo=encryption_algo, key=key)
     return initial_data
--- tests/test_ciphering_toolchain.py
+++ tests/test_ciphering_toolchain.py
@@ -3,7 +3,9 @@ import random
 import uuid
 from client.ciphering_toolchain import encrypt_video_stream, decrypt_video_stream
 
-from wacryptolib.container import LOCAL_ESCROW_PLACEHOLDER
+from wacryptolib.container import LOCAL_ESCROW_PLACEHOLDER, encrypt_data_into_container, decrypt_data_from_container
+from wacryptolib.key_generation import generate_asymmetric_keypair
+from wacryptolib.utilities import generate_uuid0
 
 SIMPLE_SHAMIR_CONTAINER_CONF = dict(
     data_encryption_strata=[
@@ -137,17 +139,33 @@ COMPLEX_SHAMIR_CONTAINER_CONF = dict(
 )
 def test_encrypt_video_stream(container_conf):
     path = "saved_video_stream/outpy.avi"
+    encryption_algo = "RSA_OAEP"
+    keypair = generate_asymmetric_keypair(key_type=encryption_algo)
+
     metadata = random.choice([None, dict(a=[123])])
     keychain_uid = random.choice(
         [None, uuid.UUID("450fc293-b702-42d3-ae65-e9cc58e5a62a")]
     )
 
     ciphered_data = encrypt_video_stream(
-        path=path, conf=container_conf, keychain_uid=keychain_uid, metadata=metadata
+        path=path, encryption_algo=encryption_algo, key=keypair["public_key"]
     )
     assert isinstance(ciphered_data, dict)
 
-    result_data = decrypt_video_stream(container=ciphered_data)
+    container_private_key = encrypt_data_into_container(
+        data=keypair["private_key"], conf=container_conf, metadata=metadata, keychain_uid=keychain_uid
+    )
+
+    assert isinstance(container_private_key, dict)
+
+    deciphered_private_key = decrypt_data_from_container(container=container_private_key)
+
+    assert deciphered_private_key == keypair["private_key"]
+
+    result_data = decrypt_video_stream(
+        cipherdict=ciphered_data, encryption_algo=encryption_algo, key=deciphered_private_key
+    )
+
     assert isinstance(result_data, bytes)
     with open(path, "rb") as video_stream:
         data = video_stream.read()
