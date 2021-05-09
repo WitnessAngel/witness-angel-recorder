361ac800503540ca264cbee51296070106dd1662
Pascal Chambon <pythoniks@gmail.com>
2021-05-09 19:56:00 +0200

    Cleanup dead/obsolete code

--- src/wanvr/app.py
+++ src/wanvr/app.py
@@ -1,28 +1,16 @@
 # Tweak logging before Kivy breaks it
-import functools
-import inspect
-
-import os, sys, logging
-
 from waguilib import kivy_presetup  # Trigger common kivy setup
+del kivy_presetup
 
 from pathlib import Path
-from uuid import UUID
-from logging.handlers import RotatingFileHandler
-from waguilib.application import WAGuiApp
-from waguilib.widgets.navigation_drawer import ItemDrawer
 
+import functools
 from kivy.clock import Clock
-from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.snackbar import Snackbar
 
+from waguilib.application import WAGuiApp
+from waguilib.widgets.navigation_drawer import ItemDrawer
 from wanvr.common import WanvrRuntimeSupportMixin
-'''
-from wanvr.rtsp_recorder.ciphering_toolchain import _generate_encryption_conf, RecordingToolchain, \
-    filesystem_key_storage_pool, \
-    filesystem_container_storage, rtsp_recordings_folder, preview_image_path, DEFAULT_FILES_ROOT
-'''
-from waguilib.logging.handlers import safe_catch_unhandled_exception
 
 WANVR_PACKAGE_DIR = Path(__file__).resolve().parent
 
@@ -33,44 +21,10 @@ WANVR_PACKAGE_DIR = Path(__file__).resolve().parent
 class WardGuiApp(WanvrRuntimeSupportMixin, WAGuiApp):  # FIXME rename this
 
     title = "Witness Angel - NVR"
-
-    #app_config_file = INTERNAL_APP_ROOT / "wanvr_config.ini"  # Might no exist yet
-    #default_config_template: str = WANVR_PACKAGE_DIR.joinpath("wardgui.ini")
-    #default_config_schema: str = WANVR_PACKAGE_DIR.joinpath("user_settings_schema.json")
-    #wip_recording_marker: str = None
-
-    dialog = None  # Any current modal dialog must be stored here
+    title_conf_panel = "NVR"
 
     app_logo_path = WANVR_PACKAGE_DIR.joinpath("logo-wa.png")
-    fallback_preview_image_path = app_logo_path
-
-    '''
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        """
-        self.CONFIG = dict(
-            data_encryption_strata=[
-                dict(
-                    data_encryption_algo="AES_CBC",
-                    key_encryption_strata=[
-                        dict(
-                            key_encryption_algo="RSA_OAEP",
-                            key_escrow=LOCAL_ESCROW_MARKER,
-                        )
-                    ],
-                    data_signatures=[
-                        dict(
-                            message_prehash_algo="SHA256",
-                            signature_algo="DSA_DSS",
-                            signature_escrow=LOCAL_ESCROW_MARKER,
-                        )
-                    ],
-                )
-            ]
-        )
-        """
-        self.recording_toolchain = None'''
+    fallback_preview_image_path = app_logo_path  # If no recording exists yet to be shown
 
     def _check_recording_configuration(self):
         shared_secret_threshold = self.get_shared_secret_threshold()
@@ -87,86 +41,7 @@ class WardGuiApp(WanvrRuntimeSupportMixin, WAGuiApp):  # FIXME rename this
 
         return True
 
-    def __________start_recording(self):
-
-        main_switch = self.screen_manager.get_screen(  # FIXME simplify
-                    "MainPage"
-                ).ids.switch
-
-        main_switch.active = False
-
-        # FIXME display popup if WRONG PARAMS!!!
-
-        # TODO call OSCP starter
-
-    '''
-    @safe_catch_unhandled_exception
-    def _stop_recording(self):
-        print(">>> started stopping recording toolchain")
-        assert self.recording_toolchain, self.recording_toolchain  # By construction...
-        self.recording_toolchain.stop_recording_toolchain_and_wait()
-        self.recording_toolchain = None
-        print(">>> finished stopping recording toolchain")
-
-    
-    @safe_catch_unhandled_exception
-    def switch_callback(self, switch_object, switch_value):  # FIXME RENAME METHOD
-        # We just swallow incoherent signals
-        if switch_value:
-            if not self.recording_toolchain:
-                self._start_recording()
-        else:
-            if self.recording_toolchain:
-                self._stop_recording()
-    '''
-
-    @property
-    def screen_manager(self):
-        if not self.root:
-            return  # Early introspection
-        return self.root.ids.screen_manager
-
-    @property
-    def selected_authentication_device_uids(self):
-        """Beware, here we lookup not the config file but the in-GUI data!"""
-        if not self.root:
-            return  # Early introspection
-        return self.screen_manager.get_screen("KeyManagement").selected_authentication_device_uids
-
-    @selected_authentication_device_uids.setter
-    def selected_authentication_device_uids(self, device_uids):
-        self.screen_manager.get_screen("KeyManagement").selected_authentication_device_uids = device_uids
-
-    @property
-    def navigation_drawer(self):
-        if not self.root:
-            return  # Early introspection
-        return self.root.ids.navigation_drawer
-
-    @property
-    def recording_button(self):
-        if not self.root:
-            return  # Early introspection
-        return self.screen_manager.get_screen("MainPage").ids.recording_button
-
-    '''
-    def build(self):
-        pass
-    '''
-    #def build_config(self, config):
-        #print(">> IN build_config")
-    #    config.setdefaults("nvr", {})
-
-
-    def build_settings(self, settings):
-        settings_file = self.config_schema_path
-        assert settings_file.exists(), settings_file
-        settings.add_json_panel("NVR", self.config, filename=settings_file)
-
-    def on_config_change(self, config, section, key, value):
-        print("CONFIG CHANGE", section, key, value)
-
-    def log_output(self, msg, *args, **kwargs):
+    def log_output(self, msg, *args, **kwargs):  # FIXME restore this
         return  # DISABLED FOR NOW
         console_output = self.screen_manager.get_screen(
             "MainPage"
@@ -174,73 +49,6 @@ class WardGuiApp(WanvrRuntimeSupportMixin, WAGuiApp):  # FIXME rename this
         console_output.add_text(
             msg
         )
-
-    def on_start(self):
-        super().on_start()
-
-        try:
-            import logging_tree
-            logging_tree.printout()
-        except ImportError:
-            pass  # Optional debug stuff
-
-
-        # Inject dependencies of loading screens
-        container_store_screen = self.screen_manager.get_screen("ContainerManagement")  # FIXME simplify
-        container_store_screen.filesystem_container_storage = self.filesystem_container_storage
-        authentication_device_store_screen = self.screen_manager.get_screen("KeyManagement")
-        authentication_device_store_screen.filesystem_key_storage_pool = self.filesystem_key_storage_pool
-
-        authentication_device_store_screen.bind(on_selected_authentication_devices_changed=self.handle_selected_authentication_device_changed)
-
-        self._insert_app_menu()
-        self.log_output("Ceci est un message de log ")
-        '''
-        self.video = Video(source="zoom_0.mp4")
-        self.video.state = "stop"
-        self.video.allow_stretch = False
-        self.root.ids.screen_manager.get_screen("MainMenu").ids.player.add_widget(
-            self.video
-        )
-        '''
-
-        self.selected_authentication_device_uids = self.load_selected_authentication_device_uids()
-
-        # create container for tests
-        # self.create_containers_for_test()
-
-        # NOW only we refresh authentication devices panel
-        #self.get_detected_devices()  # FIXME rename
-
-        Clock.schedule_interval(
-            self.update_preview_image, 30
-        )
-
-        ##self.fps_monitor_start()  # FPS display for debugging
-        '''
-        Keys_page_ids = self.root.ids.screen_manager.get_screen(  # FIXME factorize
-            "KeyManagement"
-        ).ids
-        Keys_page_ids.device_table.bind(minimum_height=Keys_page_ids.device_table.setter('height'))
-        '''
-
-    def _insert_app_menu(self):
-        screen_options = {
-            "MainPage": ("home", "Main Page"),
-            "KeyManagement": ("key", "Key Management"),
-            "ContainerManagement": ("lock", "Container Management"),
-        }
-        for screen_name, (icon_name, screen_title) in screen_options.items():
-            item_draw = ItemDrawer(icon=icon_name, text=screen_title)
-            item_draw.bind(on_release=functools.partial(self.switch_to_screen, screen_name=screen_name))
-            self.navigation_drawer.ids.content_drawer.ids.md_list.add_widget(item_draw)
-            
-    '''
-    def on_stop(self):
-        print(">>>>>> ON STOP CALLED")
-        if self.recording_toolchain:
-            self._stop_recording()  # We don't care about GUI controls since we exit anyway...
-    '''
 
     def update_preview_image(self, *args, **kwargs):
         print("We update_preview_image")
@@ -255,108 +63,88 @@ class WardGuiApp(WanvrRuntimeSupportMixin, WAGuiApp):  # FIXME rename this
         else:
             preview_image_widget.source = str(self.fallback_preview_image_path)
 
+    # GUI WIDGETS AND PROPERTIES SHORTCUTS #
+
+    @property
+    def screen_manager(self):
+        if not self.root:
+            return  # Early introspection
+        return self.root.ids.screen_manager
+
+    @property
+    def navigation_drawer(self):
+        if not self.root:
+            return  # Early introspection
+        return self.root.ids.navigation_drawer
+
+    @property
+    def selected_authentication_device_uids(self):
+        """Beware, here we lookup not the config file but the in-GUI data!"""
+        if not self.root:
+            return  # Early introspection
+        return self.screen_manager.get_screen("KeyManagement").selected_authentication_device_uids
+
+    @selected_authentication_device_uids.setter
+    def selected_authentication_device_uids(self, device_uids):
+        self.screen_manager.get_screen("KeyManagement").selected_authentication_device_uids = device_uids
+
+    @property
+    def recording_button(self): # IMPORTANT, expected by generic Screens!
+        if not self.root:
+            return  # Early introspection
+        return self.screen_manager.get_screen("MainPage").ids.recording_button
+
     def switch_to_screen(self, *args, screen_name):
         self.screen_manager.current = screen_name
         self.navigation_drawer.set_state("screen_name")
 
-    #def close_dialog(self, *args, **kwargs):
-    #    self.dialog.dismiss()
+    # KIVY APP overrides
 
-    def handle_selected_authentication_device_changed(self, event, device_uids, *args):
+    def on_config_change(self, config, section, key, value):
+        print("CONFIG CHANGE", section, key, value)
+
+    def on_start(self):
+        super().on_start()
+
+        try:
+            import logging_tree
+            logging_tree.printout()
+        except ImportError:
+            pass  # Optional debug stuff
+
+        # Inject dependencies of loading screens
+        container_store_screen = self.screen_manager.get_screen("ContainerManagement")  # FIXME simplify
+        container_store_screen.filesystem_container_storage = self.filesystem_container_storage
+        authentication_device_store_screen = self.screen_manager.get_screen("KeyManagement")
+        authentication_device_store_screen.filesystem_key_storage_pool = self.filesystem_key_storage_pool
+        authentication_device_store_screen.bind(on_selected_authentication_devices_changed=self._handle_selected_authentication_device_changed)
+
+        self._insert_app_menu()
+
+        self.selected_authentication_device_uids = self.load_selected_authentication_device_uids()
+
+        Clock.schedule_interval(
+            self.update_preview_image, 30
+        )
+
+        ##self.fps_monitor_start()  # FPS display for debugging, requires FpsMonitoring mixin
+
+    def _insert_app_menu(self):
+        screen_options = {
+            "MainPage": ("home", "Main Page"),
+            "KeyManagement": ("key", "Key Management"),
+            "ContainerManagement": ("lock", "Container Management"),
+        }
+        for screen_name, (icon_name, screen_title) in screen_options.items():
+            item_draw = ItemDrawer(icon=icon_name, text=screen_title)
+            item_draw.bind(on_release=functools.partial(self.switch_to_screen, screen_name=screen_name))
+            self.navigation_drawer.ids.content_drawer.ids.md_list.add_widget(item_draw)
+
+    def _handle_selected_authentication_device_changed(self, event, device_uids, *args):
+        """Save to config file so that the Service can access the new list"""
         self.config["nvr"]["selected_authentication_device_uids"] = ",".join(device_uids)
         self.save_config()
 
-    '''
-    def check_box_container_checked(self, radio_box_checked, value):
-        containers_page_ids = self.root.ids.screen_manager.get_screen(
-            "ContainerManagement"
-        ).ids
-        for chbx in self.check_box_container_uuid_dict:
-            if chbx.active:
-                containers_page_ids.delete.disabled = False
-                containers_page_ids.decipher.disabled = False
-                break
-            else:
-                containers_page_ids.delete.disabled = True
-                containers_page_ids.decipher.disabled = True
-
-
-    def _OBSOLETE_import_keys(self, src, dst):  # FIXME: rename this func : move_keys_directory ?
-        """
-        copy the keys of a src directory and put it in the a dst directory
-
-        """
-        target_key_storage = FilesystemKeyStorage(dst)
-        list_keypair_identifiers_destination = target_key_storage.list_keypair_identifiers()
-        source_key_storage = FilesystemKeyStorage(src)
-        list_keypair_identifiers_source = source_key_storage.list_keypair_identifiers()
-
-        for key in list_keypair_identifiers_source:
-            if key not in list_keypair_identifiers_destination:
-                public_key = source_key_storage.get_public_key(
-                    keychain_uid=key["keychain_uid"], key_type=key["key_type"]
-                )
-                if key["private_key_present"]:
-                    private_key = source_key_storage.get_private_key(
-                        keychain_uid=key["keychain_uid"], key_type=key["key_type"]
-                    )
-                target_key_storage.set_keys(
-                    keychain_uid=key["keychain_uid"],
-                    key_type="RSA_OAEP",
-                    public_key=public_key,
-                    private_key=private_key,
-                )
-
-
-
-
-    def ____create_containers_for_test(self):
-        """
-        Create 7 containers for the test
-        """
-
-        data = b"abc"
-
-        keychain_uid = generate_uuid0()
-        metadata = random.choice([None, dict(a=[123])])
-
-        if not Path(".container_storage_ward").exists():
-            Path(".container_storage_ward").mkdir()
-        for i in range(1, 7):
-            container = encrypt_data_into_container(
-                data=data,
-                conf=self.CONFIG,
-                keychain_uid=keychain_uid,
-                metadata=metadata,
-            )
-
-            container_filepath = Path(".container_storage_ward").joinpath(
-                "mycontainer" + str(i) + ".crypt"
-            )
-            dump_container_to_filesystem(
-                container_filepath, container=container, offload_data_ciphertext=False
-            )
-
-    def _OBSOLETE_list_containers_for_test(self):
-        """
-        Return a list of container and container name pair
-        """
-
-        con_stor1 = ContainerStorage(
-            default_encryption_conf=self.CONFIG, containers_dir=Path(".container_storage_ward")
-        )
-
-        list_container_name = con_stor1.list_container_names(as_sorted=True)
-        containers_list = []
-        for container_name in list_container_name:
-            container_filepath = Path(".container_storage_ward").joinpath(
-                container_name
-            )
-            container = load_container_from_filesystem(container_filepath)
-            container_and_name_container_pair = [container, container_name]
-            containers_list.append(container_and_name_container_pair)
-        return containers_list
-    '''
 
 def main():
     WardGuiApp().run()
--- src/wanvr/background_service.py
+++ src/wanvr/background_service.py
@@ -1,75 +1,24 @@
+from concurrent.futures.thread import ThreadPoolExecutor
+
 import random
+from kivy.logger import Logger as logger
 from uuid0 import UUID
 
+from wacryptolib.container import AUTHENTICATION_DEVICE_ESCROW_MARKER, SHARED_SECRET_MARKER, LOCAL_ESCROW_MARKER, \
+    ContainerStorage
 from wacryptolib.key_storage import KeyStorageBase
 from wacryptolib.sensor import TarfileRecordsAggregator, SensorsManager
-from waguilib.background_service import WaBackgroundService, osc
-
-from pathlib import Path
-
-import io
-import os
-import tarfile
-from concurrent.futures.thread import ThreadPoolExecutor
-from kivy.logger import Logger as logger
-
-from waguilib.importable_settings import INTERNAL_KEYS_DIR, EXTERNAL_DATA_EXPORTS_DIR, INTERNAL_APP_ROOT
+from waguilib.background_service import WaBackgroundService
 from wanvr.common import WanvrRuntimeSupportMixin
 from wasensorlib.camera.rtsp_stream import RtspCameraSensor
-
-'''
-from waclient.common_config import (
-    APP_CONFIG_FILE,
-    INTERNAL_KEYS_DIR,
-    EXTERNAL_DATA_EXPORTS_DIR,
-    get_encryption_conf,
-    IS_ANDROID, WIP_RECORDING_MARKER, CONTEXT)
-    '''
-from waguilib.logging.handlers import CallbackHandler, safe_catch_unhandled_exception
-from wacryptolib.container import decrypt_data_from_container, load_container_from_filesystem, \
-    AUTHENTICATION_DEVICE_ESCROW_MARKER, SHARED_SECRET_MARKER, LOCAL_ESCROW_MARKER, ContainerStorage
 
 
 class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
 
     # CLASS VARIABLES #
-
     thread_pool_executor = ThreadPoolExecutor(
         max_workers=1, thread_name_prefix="service_worker"  # SINGLE worker for now, to avoid concurrency
     )
-    #default_app_config_name = INTERNAL_APP_ROOT / "wanvr_config.ini"  # Might no exist yet
-
-    #app_config_file = INTERNAL_APP_ROOT / "wanvr_config.ini"  # Might no exist yet
-
-    @safe_catch_unhandled_exception
-    def _offloaded_attempt_container_decryption(self, container_filepath):  #FIXME move out of here
-        logger.info("Decryption requested for container %s", container_filepath)
-        target_directory = EXTERNAL_DATA_EXPORTS_DIR.joinpath(
-            os.path.basename(container_filepath)
-        )
-        target_directory.mkdir(
-            exist_ok=True
-        )  # Double exports would replace colliding files
-        container = load_container_from_filesystem(container_filepath, include_data_ciphertext=True)
-        tarfile_bytes = decrypt_data_from_container(
-            container, key_storage_pool=self._key_storage_pool
-        )
-        tarfile_bytesio = io.BytesIO(tarfile_bytes)
-        tarfile_obj = tarfile.open(
-            mode="r", fileobj=tarfile_bytesio  # TODO add gzip support here one day
-        )
-        # Beware, as root on unix systems it would apply chown/chmod
-        tarfile_obj.extractall(target_directory)
-        logger.info(
-            "Container content was successfully decrypted into folder %s",
-            target_directory,
-        )
-
-    @osc.address_method("/attempt_container_decryption")
-    @safe_catch_unhandled_exception
-    def attempt_container_decryption(self, container_filepath: str):  #FIXME move out of here
-        container_filepath = Path(container_filepath)
-        return self._offload_task(self._offloaded_attempt_container_decryption, container_filepath=container_filepath)
 
     def _get_encryption_conf(self):
         """Return a wacryptolib-compatible encryption configuration"""
@@ -125,9 +74,7 @@ class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
 
         #print(">>>>> USING ENCRYPTION CONF")
         #import pprint ; pprint.pprint(container_conf)
-
         return container_conf
-
 
     def _build_recording_toolchain(self):
 
@@ -163,7 +110,6 @@ class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
             free_keys_generator_worker=None,  # For now
         )
         return toolchain
-
 
 
 def main():
