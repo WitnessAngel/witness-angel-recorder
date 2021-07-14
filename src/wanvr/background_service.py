e2caf9042042245ef5d152a64b4a937f466cdb3d
Pascal Chambon <pythoniks@gmail.com>
2021-07-14 14:21:19 +0200

    Add PassthroughTarfileRecordsAggregator to remove some memory load when recording big videos

--- src/wanvr/background_service.py
+++ src/wanvr/background_service.py
@@ -1,6 +1,8 @@
+import os.path
 from concurrent.futures.thread import ThreadPoolExecutor
 
 import random
+import time
 from kivy.logger import Logger as logger
 from uuid0 import UUID
 
@@ -8,6 +10,7 @@ from wacryptolib.container import AUTHENTICATION_DEVICE_ESCROW_MARKER, SHARED_SE
     ContainerStorage
 from wacryptolib.key_storage import KeyStorageBase
 from wacryptolib.sensor import TarfileRecordsAggregator, SensorsManager
+from wacryptolib.utilities import synchronized
 from waguilib.background_service import WaBackgroundService
 from waguilib.importable_settings import INTERNAL_CACHE_DIR, INTERNAL_CONTAINERS_DIR
 from waguilib.logging.handlers import safe_catch_unhandled_exception
@@ -18,6 +21,25 @@ from wasensorlib.camera.rtsp_stream import RtspCameraSensor
 
 
 PREVIEW_IMAGE_PATH = INTERNAL_CACHE_DIR / "video_preview_image.jpg"
+
+
+# FIXME move this to wacryptolib
+class PassthroughTarfileRecordsAggregator(TarfileRecordsAggregator):
+
+    @synchronized
+    def add_record(self, sensor_name: str, from_datetime, to_datetime, extension: str, data: bytes):
+
+        filename = self._build_record_filename(
+            sensor_name=sensor_name, from_datetime=from_datetime, to_datetime=to_datetime, extension=extension
+        )
+        self._container_storage.enqueue_file_for_encryption(
+            filename_base=filename, data=data, metadata={}
+        )
+
+    @synchronized
+    def finalize_tarfile(self):
+        pass  # DO NOTHING
+
 
 
 class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
@@ -39,12 +61,14 @@ class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
         except ImportError:
             logger.warning("Could not import EpaperStatusDisplay, aborting setup of epaper display")
             return
-        logger.info("Setting up epaper screen and refresh button")
+        logger.info("Setting up epaper screen and refresh/on-off buttons")
         self._epaper_display = EpaperStatusDisplay()
         register_button_callback(self._epaper_display.BUTTON_PIN_1, self._epaper_status_refresh_callback)
+        register_button_callback(self._epaper_display.BUTTON_PIN_2, self._epaper_switch_recording_callback)
 
     @safe_catch_unhandled_exception
-    def _epaper_status_refresh_callback(self, *args, **kwargs):  # Might receioved pin number and such as arguments
+    def _epaper_status_refresh_callback(self, *args, **kwargs):  # Might receive pin number and such as arguments
+        logger.info("Epaper status refresh callback was triggered")
         epaper_display = self._epaper_display
         assert epaper_display, epaper_display
         epaper_display.initialize_display()
@@ -54,13 +78,28 @@ class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
         if self._recording_toolchain:
             containers_count = len(self._recording_toolchain["container_storage"])
 
+        preview_image_age_s = "Unknown"
+        try:
+            preview_image_age_s = "%ss" % int(time.time() - os.path.getmtime(PREVIEW_IMAGE_PATH))
+        except FileNotFoundError:
+            pass
+
         status_obj.update({
             "recording_status": "ON" if self.is_recording else "OFF",
             "containers": str(containers_count),
+            "Thumbnail age": preview_image_age_s
         })
 
         epaper_display.display_status(status_obj, preview_image_path=str(PREVIEW_IMAGE_PATH))
         epaper_display.release_display()
+
+    def _epaper_switch_recording_callback(self, *args, **kwargs):  # Might receive pin number and such as arguments
+        logger.info("Epaper recording switch callback  was triggered")
+        if self.is_recording:
+            self.stop_recording()
+        else:
+            self.start_recording()
+
 
     def _get_encryption_conf(self):
         """Return a wacryptolib-compatible encryption configuration"""
@@ -127,24 +166,24 @@ class WanvrBackgroundServer(WanvrRuntimeSupportMixin, WaBackgroundService):
                        containers_dir=self.internal_containers_dir,
                        key_storage_pool=self.filesystem_key_storage_pool,
                        max_workers=1, # Protect memory usage
-                       max_containers_count=12*24*1)  # 1 DAY OF DATA FOR NOW!!!
+                       max_containers_count=4*24*1)  # 1 DAY OF DATA FOR NOW!!!
 
         assert container_storage is not None, container_storage
-        tarfile_aggregator = TarfileRecordsAggregator(
+        tarfile_aggregator = PassthroughTarfileRecordsAggregator(
             container_storage=container_storage,
-            max_duration_s=5*60 - 10,  # FIXME  see get_conf_value()
+            max_duration_s=10,  # UNUSED
         )
 
         ip_camera_url = self.get_ip_camera_url()  #FIXME normalize names
 
         rtsp_camera_sensor = RtspCameraSensor(
-                interval_s=5*60,  # FIXME  see get_conf_value()
+                interval_s=15*60,  # FIXME  see get_conf_value()
                 tarfile_aggregator=tarfile_aggregator,
                 video_stream_url=ip_camera_url,
                 preview_image_path=PREVIEW_IMAGE_PATH)
 
         sensors_manager = SensorsManager(sensors=[rtsp_camera_sensor])
-
+ 
         toolchain = dict(
             sensors_manager=sensors_manager,
             data_aggregators=[],
