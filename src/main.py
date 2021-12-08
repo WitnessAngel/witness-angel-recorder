e9fe3fbfe6abdaed66171817010346593590e9a8
Pascal Chambon <pythoniks@gmail.com>
2021-12-08 23:26:15 +0100

    Check that service is not running while relaunching it

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 if __name__ == "__main__":
-    os.environ["WA_SERVICE_SCRIPT"] = str(Path(__file__).resolve().parent.joinpath("service.py"))
+    # We play it safe and ensure service is started only once
+    os.environ["WA_SERVICE_SCRIPT"] = str(Path(__file__).resolve().parent.joinpath("resurrect_service.py"))
     from waguilib.safe_launcher import launch_app_or_service
     launch_app_or_service("wanvr.app", client_type="APPLICATION")
