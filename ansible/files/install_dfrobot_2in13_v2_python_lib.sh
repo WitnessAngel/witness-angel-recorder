ef5ad14e6a49faaddc88e4fb9a999358008bc488
Pascal Chambon <pythoniks@gmail.com>
2022-10-26 23:04:39 +0200

    Add scripts to setup i2s-mic and DFRobot e-paper

---
+++ ansible/files/install_dfrobot_2in13_v2_python_lib.sh
@@ -0,0 +1,8 @@
+
+# Download and link to python3 this DFRobot driver, which is not pip-installable...
+
+mkdir -p ~/installers/
+cd ~/installers/
+git clone https://github.com/DFRobot/DFRobot_RPi_Display_V2 dfrobot_rpi_display_v2_driver
+absdir=`readlink -f ~/installers/dfrobot_rpi_display_v2_driver/`
+echo "$absdir" > ~/.venv/lib/python3.9/site-packages/dfrobot_rpi_display_v2_driver.pth
---
+++ ansible/files/install_i2smic_setup_scripts.sh
@@ -0,0 +1,6 @@
+
+
+curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py > ~/installers/i2smic_install.py
+
+
+echo -e "rm /etc/modules-load.d/snd-i2smic-rpi.conf\nrm /etc/modprobe.d/snd-i2smic-rpi.conf\necho 'To enable i2smic manually: sudo modprobe snd-i2smic-rpi rpi_platform_generation=<0 for pizero, 1 for Pi 2/3, or 2 for Pi 4>'" > ~/installers/i2smic_disable_on_boot.sh
