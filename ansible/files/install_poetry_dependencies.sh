1768b9fc5f58b060b0ab5520e754d9e36fd5ad4f
Pascal Chambon <pythoniks@gmail.com>
2022-11-29 10:08:30 +0100

    Fix order of commands in install_poetry_dependencies.sh

--- ansible/files/install_poetry_dependencies.sh
+++ ansible/files/install_poetry_dependencies.sh
@@ -4,5 +4,6 @@ source ~/.venv/bin/activate
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 # Beware, keep that in sync with Kivy version of pyproject.toml, we must install it without multiprocessing else RAM explodes
 ##pip install --global-option="-j1" Kivy==2.0.0 - nope doesn't work
-poetry install --no-ansi
 poetry config installer.max-workers 1  # Protect RAM limits
+poetry install --no-ansi
+
