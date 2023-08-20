13b784af49ad376876db3a0d49b51dfdaeda6923
Pascal Chambon <pythoniks@gmail.com>
2023-08-20 11:49:48 +0200

    Fix update_i18n.sh

--- update_i18n.sh
+++ update_i18n.sh
@@ -1,6 +1,6 @@
-find src/warecorder/ -type f \( -name '*.py' -or -name '*.kv' \)  -print > __translatable_files
-xgettext --from-code=UTF-8 --files-from=__translatable_files -Lpython -o src/warecorder/locale/_messages.pot
-msgmerge --update --backup=off src/warecorder/locale/fr/LC_MESSAGES/witnessangel.po src/warecorder/locale/_messages.pot
-msgfmt -c -o src/warecorder/locale/fr/LC_MESSAGES/witnessangel.mo src/warecorder/locale/fr/LC_MESSAGES/witnessangel.po
+find warecorder/ -type f \( -name '*.py' -or -name '*.kv' \)  -print > __translatable_files
+xgettext --from-code=UTF-8 --files-from=__translatable_files -Lpython -o warecorder/locale/_messages.pot
+msgmerge --update --backup=off warecorder/locale/fr/LC_MESSAGES/witnessangel.po warecorder/locale/_messages.pot
+msgfmt -c -o warecorder/locale/fr/LC_MESSAGES/witnessangel.mo warecorder/locale/fr/LC_MESSAGES/witnessangel.po
 rm __translatable_files
 
