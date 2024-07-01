d67b9d557c9720c3b17f0f7f44ebc00187efd3d7
Pascal Chambon <pythoniks@gmail.com>
2024-07-01 21:04:33 +0200

    Switch to GPLv2-or-later license

---
+++ warecorder/__init__.py
@@ -0,0 +1,3 @@
+# This file is part of Witness Angel Recorder
+# SPDX-FileCopyrightText: Copyright Prolifik SARL
+# SPDX-License-Identifier: GPL-2.0-or-later
--- warecorder/warecorder_gui.py
+++ warecorder/warecorder_gui.py
@@ -1,3 +1,6 @@
+# This file is part of Witness Angel Recorder
+# SPDX-FileCopyrightText: Copyright Prolifik SARL
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 from kivy.resources import resource_find
 import logging
