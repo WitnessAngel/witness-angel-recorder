bccbbbf0bd6838b9e43ec8f33dfd1b704c5f3416
Pascal Chambon <pythoniks@gmail.com>
2025-08-08 14:05:40 +0200

    Improve efficiency and robustness of ansible workflow

--- ansible/files/install_dfrobot_2in13_v2_python_lib.sh
+++ ansible/files/install_dfrobot_2in13_v2_python_lib.sh
@@ -3,6 +3,6 @@
 
 mkdir -p ~/installers/
 cd ~/installers/
-git clone https://github.com/DFRobot/DFRobot_RPi_Display_V2 dfrobot_rpi_display_v2_driver
+git clone https://github.com/DFRobot/DFRobot_RPi_Display_V2 dfrobot_rpi_display_v2_driver --single-branch --depth 1
 absdir=`readlink -f ~/installers/dfrobot_rpi_display_v2_driver/`
 echo "$absdir" > ~/.venv/lib/python3.9/site-packages/dfrobot_rpi_display_v2_driver.pth
