38a5a7b980aeab4d7aa7800bb3eab4efb060f921
Pascal Chambon <pythoniks@gmail.com>
2025-09-20 17:45:34 +0200

    Update ansible configurator recipe for RPi OS Bookworm/Python3.11

--- ansible/files/install_dfrobot_2in13_v2_python_lib.sh
+++ ansible/files/install_dfrobot_2in13_v2_python_lib.sh
@@ -5,4 +5,4 @@ mkdir -p ~/installers/
 cd ~/installers/
 git clone https://github.com/DFRobot/DFRobot_RPi_Display_V2 dfrobot_rpi_display_v2_driver
 absdir=`readlink -f ~/installers/dfrobot_rpi_display_v2_driver/`
-echo "$absdir" > ~/.venv/lib/python3.9/site-packages/dfrobot_rpi_display_v2_driver.pth
+echo "$absdir" > ~/.venv/lib/python3.11/site-packages/dfrobot_rpi_display_v2_driver.pth
--- ansible/files/install_waveshare_lcd_examples.sh
+++ ansible/files/install_waveshare_lcd_examples.sh
@@ -1,4 +1,4 @@
-wget https://www.waveshare.com/w/upload/b/bd/1.3inch_LCD_HAT_code.7z -O /tmp/1.3inch_LCD_HAT_code.7z
+wget https://files.waveshare.com/upload/b/bd/1.3inch_LCD_HAT_code.7z -O /tmp/1.3inch_LCD_HAT_code.7z
 7z x /tmp/1.3inch_LCD_HAT_code.7z -o/tmp/1.3inch_LCD_HAT_code_7z
 mv /tmp/1.3inch_LCD_HAT_code_7z/1.3inch_LCD_HAT_code ~/installers/waveshare_1.3inch_lcd_hat_code
 rm ~/installers/waveshare_1.3inch_lcd_hat_code/fbtft/letitgo.mp4
